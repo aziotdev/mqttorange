2833c567e3a15b3ac119b90ad42500636d3a9780
jayaruvan <jayaruvan@gmail.com>
2019-07-07 00:39:42 +0530

    mqtt_price

---
+++ mqtt_price.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+#
+# !!! Needs psutil (+ dependencies) installing:
+#
+#    $ sudo apt-get install python-dev
+#    $ sudo pip install psutil
+#
+
+from datetime import datetime
+from oled.device import ssd1306, sh1106
+from oled.render import canvas
+from PIL import ImageFont
+import paho.mqtt.client as mqtt
+
+
+
+oled = ssd1306(port=0, address=0x3C)
+
+font = ImageFont.load_default()
+font2 = ImageFont.truetype('C&C Red Alert [INET].ttf', 14)
+
+ 
+MQTT_SERVER = "mqtt.aziot.org"
+MQTT_PATH = "PRICE"
+ 
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+ 
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe(MQTT_PATH)
+ 
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+    with canvas(oled) as draw:
+        draw.text((0, 0), "Channel "+msg.topic , font=font2, fill=255)
+        draw.text((0, 18), "Item ", font=font2, fill=255)
+        draw.text((0, 32), "Price ", font=font2, fill=255)
+        #draw.text((0, 38), network('wlan0'), font=font2, fill=255)
+ 
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+ 
+client.connect(MQTT_SERVER, 1883, 60)
+ 
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+client.loop_forever()
+
+
+
