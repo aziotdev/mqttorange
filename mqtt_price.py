d7b3a834de2aa9c93652c6754a56ac32a06415cd
jayaruvan <jayaruvan@gmail.com>
2019-07-08 18:54:24 +0530

    one line

--- mqtt_price.py
+++ mqtt_price.py
@@ -19,8 +19,7 @@ oled = ssd1306(port=0, address=0x3C)
 font = ImageFont.load_default()
 font2 = ImageFont.truetype('C&C Red Alert [INET].ttf', 18)
 font4 = ImageFont.truetype('C&C Red Alert [INET].ttf', 28)
-font6 = ImageFont.truetype('C&C Red Alert [INET].ttf', 36)
-
+ 
 MQTT_SERVER = "mqtt.aziot.org"
 MQTT_PATH = "PRICE"
  
@@ -48,18 +47,9 @@ def on_message(client, userdata, msg):
 
         if(len(x)==1):
                 #print(" length ==1")
-                y = x[0].split("-")
                 with canvas(oled) as draw:
                         draw.text((0, 0), msg.topic , font=font2, fill=255)
-                        draw.text((0, 22), x[1], font=font4, fill=255)
-                        
-#                        if (len(y)==2):
-#                                draw.text((0, 22), y[0], font=font4, fill=255)
-#                                draw.text((0, 55), y[1], font=font4, fill=255)
-#                        if (len(y)==1):
-#                                draw.text((0, 22), x[1], font=font4, fill=255)
-#                                #draw.text((0, 55), y[1], font=font6, fill=255)
-
+                        draw.text((0, 22), x[0], font=font4, fill=255)
 
 client = mqtt.Client()
 client.on_connect = on_connect
